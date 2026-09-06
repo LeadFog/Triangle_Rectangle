04718a113ab1f4e7d3edb95a52cea76250f7e8a0
MSI <MSI@MSI-PC47>
2026-09-06 18:59:57 +0600

    Добавьте файлы проекта.

---
+++ Triangle_Rectangle/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Triangle_Rectangle
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

ccaa8842cc2767d898d6651ecb9ddb6515d2ac12
MSI <10xsky_ferx10@mail.ru>
2026-09-06 19:41:29 +0600

    Добавлен класс Point

---
+++ Triangle_Rectangle/Line.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle_Rectangle
+{
+    internal class Line
+    {
+    }
+}
---
+++ Triangle_Rectangle/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle_Rectangle
+{
+    public class Point
+    {
+        public int X {  get; private set; }
+        public int Y { get; private set; }
+
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void AddX(int x)
+        {
+            X += x;
+        }
+
+        public int AddY(int y) 
+        {
+            Y += y; 
+        }
+    }
+}
